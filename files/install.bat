033f3f45266f81c6b58299c8c6a6505a704d4964
ThePyGoose <87911411+ThePyGoose@users.noreply.github.com>
2021-08-01 23:32:00 +0400

    Update install.bat

--- files/install.bat
+++ files/install.bat
@@ -1,7 +1,7 @@
 @echo off
 net config server /srvcomment:"Windows Server 2016"
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F
-net user administrator administator /add >nul
+net user administrator @admin69 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -14,7 +14,6 @@ echo RDP is Installed
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: admin
-echo .
-echo  RDP is Online
+echo Password: @admin69
+echo  RDP is Launch
 ping -n 10 127.0.0.1 >nul
