112847fde314bbbf28a570bac8558b0fcd009351
ThePyGoose <87911411+ThePyGoose@users.noreply.github.com>
2021-08-02 23:02:27 +0400

    Update exit.bat

--- files/exit.bat
+++ files/exit.bat
@@ -7,6 +7,7 @@ echo Password: @admin69
 tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels " & ping 127.0.0.1 >Nul & exit
 echo "You are using the repository from github.com/ThePyGoose/KryptexCore"
 :check
+echo "Репозиторий KryptexCore - https://github.com/ThePyGoose/KryptexCore/"
 ping 127.0.0.1 > nul
 cls
 
