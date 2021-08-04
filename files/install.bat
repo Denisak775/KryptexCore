35d4af5f9f00f3991949fb8eec122222beb1288e
ThePyGoose <87911411+ThePyGoose@users.noreply.github.com>
2021-08-04 12:54:55 +0400

    Update install.bat

--- files/install.bat
+++ files/install.bat
@@ -13,7 +13,7 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo RDP is Installed 
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
+echo Official KryptexCore: https://github.com/ThePyGoose/KryptexCore
 echo Username: administrator
 echo Password: @admin69
-echo  RDP is Launch
 ping -n 10 127.0.0.1 >nul
